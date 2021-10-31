2e99aa22b00cea139a3b02a849f088ddf418f0a8
Mina Zakhary <mina.zakhary@my.jcu.edu.au>
2021-11-01 01:54:01 +1000

    complete cleanup_files.py and sort_files_1.py

--- SEM_2_PRACTICALS/prac_09/cleanup_files.py
+++ SEM_2_PRACTICALS/prac_09/cleanup_files.py
@@ -40,9 +40,34 @@ def main():
         # shutil.move(filename, 'temp/' + new_name)
 
 
+# def get_fixed_filename(filename):
+#     """Return a 'fixed' version of filename."""
+#     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+#     return new_name
+
+
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    draft_name = ((filename.split('.'))[0]).replace("_", "")
+    name_parts = "".join([(" " + i if i.isupper() else i) for i in draft_name]).strip().split()
+    new_name_parts = []
+    new_name = ""
+    for name in name_parts:
+        new_word = ""
+        for character in name:
+            if name.index(character) == 0 or name[name.index(character) - 1] in ["(", "_"]:
+                new_word += character.title()
+            else:
+                new_word += character
+        new_name_parts.append(new_word)
+    for index, name in enumerate(new_name_parts):
+        if index != 0 and str(new_name_parts[index - 1]) != "(":
+            new_name += "_" + name
+        elif str(new_name_parts[index - 1]) == "(":
+            new_name += name
+        else:
+            new_name += name
+    new_name += '.txt'
     return new_name
 
 
@@ -55,12 +80,22 @@ def demo_walk():
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        for subdirectory in subdirectories:
-            for filename in filenames:
-                print(os.path.join(directory_name, filename))
-                new_name = get_fixed_filename(filename)
-                os.rename(directory_name/subdirectory/filename, directory_name/subdirectory/new_name)
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            os.rename(f'.\{directory_name}\{filename}', f'.\{directory_name}\{new_name}')
 
+
+def cleanup_files():
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            os.rename(f'.\{directory_name}\{filename}', f'.\{directory_name}\{new_name}')
 
 # main()
-demo_walk()
+# demo_walk()
+cleanup_files()
